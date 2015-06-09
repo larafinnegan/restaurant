568424faaf0eda25e116c2d4b493ea6f208fff36
Lara Finnegan <lcfinnegan@gmail.com>
2015-06-09 19:33:38 -0400

    changed the price column back to a decimal format with precision 5 and scale 2

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 FactoryGirl.define do 
   factory :item do
-    title { Faker::Lorem.words(4) }
+    title { Faker::Lorem.sentence}
     description { Faker::Lorem.paragraph}
     price { Faker::Commerce.price }
   end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,6 +7,30 @@ RSpec.describe Item, type: :model do
     expect(FactoryGirl.build(:item)).to be_valid
   end
 
-  it "has a valid title"
-  it "has a valid description"
+  it "cannot have an empty title" do 
+    expect(FactoryGirl.build(:item, title: "   ")).not_to be_valid
+  end
+
+   it "cannot have an empty description" do 
+    expect(FactoryGirl.build(:item, description: "   ")).not_to be_valid
+  end
+
+   it "cannot have a price of 0" do 
+    expect(FactoryGirl.build(:item, price: 0.00)).not_to be_valid
+  end
+
+  it "cannot have a price less than 0" do 
+    expect(FactoryGirl.build(:item, price: -1.00)).not_to be_valid
+  end
+
+   it "has a unique title" do
+      new_item = FactoryGirl.create(:item)
+      expect(FactoryGirl.build(:item, title: new_item.title)).not_to be_valid
+  end
+
+  it "price must have 2 decimal places" do
+      item = FactoryGirl.create(:item)
+      p item.price
+      expect(FactoryGirl.build(:item, price: 5.002)).to eql(5.00)
+  end
 end
