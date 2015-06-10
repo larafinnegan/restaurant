d1f301d920bd80b4ad42290c7541119ce3af0f8c
Lara Finnegan <lcfinnegan@gmail.com>
2015-06-09 22:45:10 -0400

    item model validation to ensure items have at least one category

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,12 @@ class Item < ActiveRecord::Base
   has_many :items_categories
   has_many :categories, -> { uniq }, through: :items_categories
 
+
   validate :decimal_places
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01, less_than_or_equal_to: 999.99 }
+  validates :categories, length: { minimum: 1 }
 
   def decimal_places
     if price.to_s.split(".")[1] && price.to_s.split(".")[1].length > 2
