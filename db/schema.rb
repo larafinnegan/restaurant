2707e1276f2acd88c4276eaefaf8b7fb34413dd8
Lara Finnegan <lcfinnegan@gmail.com>
2015-06-11 18:54:17 -0500

    created user model and model validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611004818) do
+ActiveRecord::Schema.define(version: 20150611233731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,5 +41,14 @@ ActiveRecord::Schema.define(version: 20150611004818) do
   end
 
   add_index "items", ["title"], name: "index_items_on_title", unique: true, using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "display_name"
+    t.string   "email"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    first_name "MyString"
+last_name "MyString"
+display_name "MyString"
+email "MyString"
+  end
+
+end
